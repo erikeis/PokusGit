2c9fff163d603d8f2c4cf4ec5978f5a49ca76869
erikeis <eise.05@A321-15>
2022-03-04 11:22:09 +0100

    odcitani

--- PokusGit/Form1.cs
+++ PokusGit/Form1.cs
@@ -24,5 +24,21 @@ namespace PokusGit
             int soucet = x + y;
             MessageBox.Show("Soucet cisel je " + soucet.ToString());
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+            int rozdil = x - y;
+            MessageBox.Show("Rozdil cisel je " + rozdil.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            int y = int.Parse(textBox2.Text);
+            int soucin = x * y;
+            MessageBox.Show("Soucin cisel je " + soucin.ToString());
+        }
     }
 }
